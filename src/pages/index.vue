cc6f057be0c88e246067f2e1651c4af3982e52cf
Nick Glyzhko <nick4frm@gmail.com>
2018-08-28 13:27:52 +0300

    Initial

---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Test: {{def}}
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      def: "test"
+    };
+  }
+  // mounted() {
+
+  // },
+  // methods: {
+
+  // },
+  // computed: {
+
+  // },
+  // watch: {
+
+  // }
+};
+</script>
+
+<style lang="scss">
+</style>
+
