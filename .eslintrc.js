069dc277ee241431123257931ae513869dc7f9e4
Nick Glyzhko <nick4frm@gmail.com>
2018-08-28 14:21:30 +0300

    add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'space-before-function-paren': 0,
+    'no-undef': 0,
+    'no-new': 0,
+    'template-tag-spacing': 1,
+    "semi": 0,
   },
   parserOptions: {
     parser: 'babel-eslint'
